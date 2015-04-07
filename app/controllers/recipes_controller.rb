0c44c80b353003229958c63300fdc7868ebb4433
eric bartsch <bartsched@gmail.com>
2015-04-07 15:16:33 -0600

    add yummly api and search functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,6 @@
 class RecipesController < ApplicationController
-  
+  def index
+    yummly_api = YummlyAPI.new
+    @recipes = yummly_api.recipes(params[:search])
+  end
 end
--- app/models/yummly_api.rb
+++ app/models/yummly_api.rb
@@ -1,15 +1,14 @@
 class YummlyAPI
 
   def initialize
-    @conn = Faraday.new(:url => 'http://api.yummly.com')
+    @conn = Faraday.new(:url => "http://api.yummly.com/v1/api/recipes?_app_id=5b570d0a&_app_key=9f92ef3b82c056281393cbe1996ad439&q=street food")
   end
 
-  def recipes(app_id, app_key)
+  def recipes(search)
     response = @conn.get do |req|
-      req.url "/v1"
+      req.url "http://api.yummly.com/v1/api/recipes?_app_id=5b570d0a&_app_key=9f92ef3b82c056281393cbe1996ad439&q=street food+#{search}"
       req.headers['Content-Type'] = 'application/json'
-      req.headers["X-Yummly-App-ID"] = app_id
-      req.headers["X-Yummly-App-Key"] = app_key
+      req.headers['search'] = 'search'
     end
     JSON.parse(response.body, symbolize_names: true)
   end
